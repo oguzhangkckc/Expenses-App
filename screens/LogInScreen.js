9519137a86f06f824ec4a22537b7fd427156d551
Oğuzhan Gökcekoca <ogokcekoca@gmail.com>
2023-02-27 17:14:58 +0300

    düzenleme

--- screens/LogInScreen.js
+++ screens/LogInScreen.js
@@ -83,7 +83,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#cfeffd",
-    paddingTop: 200,
+    paddingTop: 180,
     alignItems: "center",
   },
   login: {
